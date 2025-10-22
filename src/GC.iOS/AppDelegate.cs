7e1aeece1c50a08f128e71b509ecc03417b6aa80
radaiko <a.radlingmayr@icloud.com>
2025-10-22 20:26:31 +0200

    add missing files

---
+++ src/GC.iOS/AppDelegate.cs
@@ -0,0 +1,8 @@
+using Foundation;
+
+namespace GC.iOS;
+
+[Register(nameof(AppDelegate))]
+public class AppDelegate : MauiUIApplicationDelegate {
+  protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
---
+++ src/GC.iOS/MauiProgram.cs
@@ -0,0 +1,15 @@
+﻿using GC.Frontend.Mobile;
+using Microsoft.Maui.Hosting;
+
+namespace GC.iOS;
+
+public static class MauiProgram {
+  public static MauiApp CreateMauiApp() {
+    var builder = MauiApp.CreateBuilder();
+
+    builder
+      .UseSharedMauiApp();
+
+    return builder.Build();
+  }
+}
