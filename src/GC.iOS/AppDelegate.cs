cb9353bbd0f7b2150d63bd7945548079ea0e62d4
radaiko <a.radlingmayr@icloud.com>
2025-10-24 08:23:49 +0200

    //WP create custom controls to easy modify design in the future

--- src/GC.iOS/AppDelegate.cs
+++ src/GC.iOS/AppDelegate.cs
@@ -6,5 +6,13 @@ namespace GC.iOS;
 
 [Register(nameof(AppDelegate))]
 public class AppDelegate : MauiUIApplicationDelegate {
-  protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+  protected override MauiApp CreateMauiApp() {
+    // Remove borders from Entry controls to have ios26 style
+    Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("SetupEntry", (handler, view) => {
+      if (view is Microsoft.Maui.Controls.Entry entry) {
+        handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+      }
+    });
+    return MauiProgram.CreateMauiApp();
+  }
 }
