2281a51404280fa757c4d8b17d919ea77b5ecdea
radaiko <a.radlingmayr@icloud.com>
2026-02-12 13:04:30 +0100

    Sync web scrollbar color scheme with app dark mode

--- src/app/app/_layout.tsx
+++ src/app/app/_layout.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { Platform } from 'react-native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 // IMPORTANT: tauriHttp must be imported BEFORE any store modules.
@@ -7,15 +8,23 @@ import { StatusBar } from 'expo-status-bar';
 import '../src-rn/utils/tauriHttp';
 import { useAuthStore } from '../src-rn/store/authStore';
 import { useVentopayAuthStore } from '../src-rn/store/ventopayAuthStore';
+import { useTheme } from '../src-rn/theme/useTheme';
 
 export default function RootLayout() {
   const gourmetLoginWithSaved = useAuthStore((s) => s.loginWithSaved);
   const ventopayLoginWithSaved = useVentopayAuthStore((s) => s.loginWithSaved);
+  const { colorScheme } = useTheme();
 
   useEffect(() => {
     gourmetLoginWithSaved();
     ventopayLoginWithSaved();
   }, [gourmetLoginWithSaved, ventopayLoginWithSaved]);
+
+  useEffect(() => {
+    if (Platform.OS === 'web') {
+      document.documentElement.style.colorScheme = colorScheme;
+    }
+  }, [colorScheme]);
 
   return (
     <>
